638f68d68016fa88b751dad16ea76ddafe9c6392
gepd <guillermoepd@hotmail.com>
2023-12-01 12:43:41 -0300

    📦 update compiled files

--- dist/checkTimeRange.js
+++ dist/checkTimeRange.js
@@ -8,6 +8,8 @@ var checkTimeRange = function (data, session, timezone) {
     var startDate = data.start, endDate = data.end;
     var startDateLocal = luxon_1.DateTime.fromISO(startDate).setZone(timezone);
     var endDateLocal = luxon_1.DateTime.fromISO(endDate).setZone(timezone);
+    startDateLocal.set({ second: 0, millisecond: 0 });
+    endDateLocal.set({ second: 0, millisecond: 0 });
     var _a = startSession.split(":").map(Number), startHour = _a[0], startMinute = _a[1];
     var _b = endSession.split(":").map(Number), endHour = _b[0], endMinute = _b[1];
     var startSessionDate = luxon_1.DateTime.fromObject({
@@ -16,6 +18,8 @@ var checkTimeRange = function (data, session, timezone) {
         day: startDateLocal.day,
         hour: startHour,
         minute: startMinute,
+        second: 0,
+        millisecond: 0,
     }, { zone: timezone });
     var endSessionDate = luxon_1.DateTime.fromObject({
         year: endDateLocal.year,
@@ -23,6 +27,8 @@ var checkTimeRange = function (data, session, timezone) {
         day: endDateLocal.day,
         hour: endHour,
         minute: endMinute,
+        second: 0,
+        millisecond: 0,
     }, { zone: timezone });
     return startDateLocal >= startSessionDate && endDateLocal <= endSessionDate;
 };
