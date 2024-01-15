37117cd6e717ce9ddfff8643e3287ef268c6e84f
gepd <guillermoepd@hotmail.com>
2024-01-15 11:13:42 -0300

    Fix function to calculate inTime function

--- src/isInTime.ts
+++ src/isInTime.ts
@@ -3,26 +3,23 @@ import { DateTime } from "luxon";
 export type Direction = "before" | "after";
 
 export const isInTime = (
-  utcDate: Date,
+  utcDate: string,
   targetTime: string,
   direction: Direction,
   zone: string
 ): boolean => {
-  const dateInUtc = DateTime.fromJSDate(utcDate, { zone: "utc" });
+  const dateInUtc = DateTime.fromISO(utcDate, { zone: "utc" });
 
   const [hours, minutes] = targetTime.split(":").map(Number);
-  const targetDate = DateTime.fromObject(
-    {
-      year: dateInUtc.year,
-      month: dateInUtc.month,
-      day: dateInUtc.day,
-      hour: hours,
-      minute: minutes,
-    },
-    {
-      zone,
-    }
-  ).toUTC();
+  const targetDate = DateTime.fromObject({
+    year: dateInUtc.year,
+    month: dateInUtc.month,
+    day: dateInUtc.day,
+    hour: hours,
+    minute: minutes,
+  })
+    .setZone(zone)
+    .toUTC();
 
   if (direction === "before") {
     return dateInUtc <= targetDate;
