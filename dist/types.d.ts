68b3a288ed1bfcee18a3a3b45cbb0c34e90ddddc
gepd <guillermoepd@hotmail.com>
2024-01-30 10:47:04 -0300

    update compiled files

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -2,31 +2,39 @@ import { Models } from "./models";
 /**
  * DATABASE TYPES
  */
-export interface HomeCard {
+export type HomeCard = {
     title: string;
     body: string;
     image: string;
     category: string;
-}
+};
 export type HomeCardProps = Models.Document & HomeCard;
-export interface Profile {
+export type Profile = {
     name: string;
     pid: string;
     avatar?: string;
     title?: string;
     mobile?: string;
-}
+    departmentId?: string;
+    department?: Department;
+};
 export type ProfileProps = Models.Document & Profile;
-export interface Neighborhood {
+export type Department = {
     name: string;
-}
+    short_name: string;
+    parentId?: string;
+};
+export type DepartmentProps = Models.Document & Department;
+export type Neighborhood = {
+    name: string;
+};
 export type NeighborhoodProps = Models.Document & Neighborhood;
-export interface Christmas {
+export type Christmas = {
     user: UserProps;
     pid: string;
-}
+};
 export type ChristmasProps = Models.Document & Christmas;
-export interface User {
+export type User = {
     name: string;
     firstname: string;
     lastname: string;
@@ -41,17 +49,17 @@ export interface User {
     email?: string;
     phone?: string;
     registeredBy?: string;
-}
+};
 export type UserProps = Models.Document & User;
 export type UserTypeForm = Omit<User, "birthday" | "neighborhood"> & {
     birthday: Date;
     neighborhood?: string;
 };
-export interface Menu {
+export type Menu = {
     key: string;
-}
+};
 export type MenuProps = Models.Document & Menu;
-export interface Lunch {
+export type Lunch = {
     profile: ProfileProps;
     start: string;
     end?: string;
@@ -60,30 +68,30 @@ export interface Lunch {
     isInRange: boolean;
     hoursExtended: number;
     minutesExtended: number;
-}
+};
 export type LunchProps = Models.Document & Lunch;
 export declare enum OvertimeTypes {
     TYPE_25 = "TYPE_25",
     TYPE_50 = "TYPE_50"
 }
-export interface Overtime {
+export type Overtime = {
     profile: ProfileProps;
     start: string;
     end?: string;
     hours: number;
     minutes: number;
     type: OvertimeTypes;
-}
+};
 export type OvertimeProps = Models.Document & Overtime;
-export interface Markin {
+export type Markin = {
     profile: ProfileProps;
     profileId: string;
     workday: string;
     date: string;
     isComputed: boolean;
-}
+};
 export type MarkinProps = Models.Document & Markin;
-export interface Workday {
+export type Workday = {
     profile: ProfileProps;
     start: string;
     end?: string;
@@ -94,60 +102,60 @@ export interface Workday {
     lunch?: LunchProps;
     overtime: OvertimeProps[];
     markin: MarkinProps[];
-}
+};
 export type WorkdayProps = Models.Document & Workday;
-export interface Holliday {
+export type Holliday = {
     name: string;
     date: string;
-}
+};
 export type HollidayProps = Models.Document & Holliday;
-export interface ClockMarkinOptionProps {
+export type ClockMarkinOptionProps = {
     isWorkingDay: boolean;
     isLeaveDay: boolean;
-}
+};
 export type formatArgumentType = string | Date | number | boolean;
-export interface WorkCount {
+export type WorkCount = {
     profileId: string;
     type: string;
     date: string;
     hours: number;
     minutes: number;
-}
+};
 export type WorkCountProps = Models.Document & WorkCount;
-export interface Configuration {
+export type Configuration = {
     key: string;
     option: string;
-}
+};
 export type ConfigurationProps = Models.Document & Configuration;
 /**
  * PROYECT TYPES
  */
-export interface FilterProps {
+export type FilterProps = {
     column: SelectOptionProps;
     operator: SelectOptionProps;
     value: formatArgumentType;
     optional: formatArgumentType;
-}
-export interface FilterFormProps {
+};
+export type FilterFormProps = {
     column: string;
     operator: string;
     value: formatArgumentType;
     optional: formatArgumentType;
-}
-export interface TimeHoursMinutes {
+};
+export type TimeHoursMinutes = {
     hours: number;
     minutes: number;
-}
-export interface SelectOptionProps {
+};
+export type SelectOptionProps = {
     label: string;
     value: string;
-}
-export interface addMarkin {
+};
+export type addMarkin = {
     $id: string;
     date: string | Date;
     isClock: boolean;
-}
-export interface SchedulesProps {
+};
+export type SchedulesProps = {
     name: string;
     type: string;
     dayStart: number;
@@ -156,33 +164,79 @@ export interface SchedulesProps {
         start: string;
         end: string;
     };
-}
-export interface HoursAndExtended {
+};
+export type HoursAndExtended = {
     hours: number;
     minutes: number;
     hoursExtended: number;
     minutesExtended: number;
     totalHours: number;
     totalMinutes: number;
-}
-export interface Period {
+};
+export type Period = {
     start: string;
     end: string;
-}
-export interface ServerSession {
+};
+export type ServerSession = {
     cookie: string;
     session: {
         account: Models.User<Models.Preferences> | null;
         menu: MenuProps[];
     };
-}
-export interface TimeObjectProps {
+};
+export type TimeObjectProps = {
     hours: number;
     minutes: number;
     start: string;
     end: string;
-}
-export interface TimeToSubtract {
+};
+export type TimeToSubtract = {
     hours: number;
     minutes: number;
-}
+};
+/** FINANCE */
+export type FinanceRequest = {
+    requestId: string;
+    departmentId: string;
+    documentType: string;
+    classification: string;
+    content?: string;
+    purchaseType?: string;
+    managerId?: string;
+    amount: number;
+    publishingDate: string;
+    awairdId: string;
+    awardDate: string;
+    supplierId: string;
+    purchaseOrderId: string;
+    purchaseOrderDate: string;
+    purchaseOrderLink: string;
+};
+export type FinanceRequestProps = FinanceRequest & Models.Document;
+export type FinanceTracking = {
+    requestId: string;
+    submit: string;
+    departmentId: string;
+    note: string;
+};
+export type FinanceTrackingProps = FinanceTracking & Models.Document;
+export type FinanceTimeline = {
+    profileId: string;
+    eventName: string;
+    additional: string;
+};
+export type FinanceTimelineProps = FinanceTimeline & Models.Document;
+export type FinanceSupplier = {
+    pid: string;
+    name: string;
+    mobile?: string;
+    phone?: string;
+    email?: string;
+    accountNumber?: string;
+    accountBank?: string;
+    emailNotification?: string;
+    emailSubjet?: string;
+    legalRepresentativeName?: string;
+    legalRepresentativePid?: string;
+};
+export type FinanceSupplierProps = FinanceSupplier & Models.Document;
